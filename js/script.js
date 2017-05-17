6cef7781779123c5031e8c0d6566dcbdeb2d8f51
cyryl91 <91.cyryl@gmail.com>
2017-05-17 20:04:54 +0200

    beta versia

--- js/script.js
+++ js/script.js
@@ -1,64 +1,44 @@
 $(function(){
-/*
 
 var carouselList = $("#carousel ul");
 
-function timeNext(){
-	setInterval(changeSliden, 3000);
-}
+var prev = $('body').find('.btnP'),
+	next = $('body').find('.btnN');
 
-function changeSliden(){
-	carouselList.animate({'marginLeft':-600}, 2000, moveFirstSlide);
-	
-	}
-
-function moveFirstSlide(){
-
-	var firstItem = carouselList.find("li:first"),
-		lastItem = carouselList.find("li:last");
-
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-}
-
-
-timeNext();
-
-
-*/
-var buttonPrivers = $('js-btnP'),
-	buttonNext = $('js-btnN'),
-	btn = $('button');
-
-var carouselList = $("#carousel ul");
-
-$('#js-btnP').click(function(){
-	wPrawo();
-});
-$('#js-btnN').click(function(){
-	wLewo();
-});
-console.log(btn);
 
 
 function wPrawo(){
-	carouselList.animate({'marginLeft':-600}, 2000, moveFirstSlide);
+	
+	var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");  
 
+
+	lastItem.after(firstItem);  
+	carouselList.css({marginLeft: 0}); 
 }
 
 function wLewo(){
-	carouselList.animate({'marginLeft':600}, 2000, moveFirstSlide);
-}
-
-function moveFirstSlide(){
-
+	
 	var firstItem = carouselList.find("li:first"),
 		lastItem = carouselList.find("li:last");
 
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
+	
+	firstItem.before(lastItem);
+	carouselList.css({marginLeft: 0}); 
 }
+
+$(prev).click(function(){
+	console.log('wLewo');
+	carouselList.animate({'marginLeft': 600}, 1000, wLewo);
+	
+});
+
+$(next).click(function(){
+	console.log('wPrawo');
+	carouselList.animate({'marginLeft': -600}, 1000, wPrawo);
+	
+});
+
 
 
 });
-
